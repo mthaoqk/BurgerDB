2545ed93eac1b027d1251637d0384da629b1e0c5
Mthao09 <mthaoqk@users.noreply.github.com>
2018-07-27 08:19:10 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require('mysql');
+var connection;
 
-	var connection = mysql.createConnection({
-		port: 3306,
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'burgers_db'
-	})
-connection.connect(function(err) {
-  console.log('Connected to Port ' + connection.threadId + '\n\n');
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "root",
+		database: "burgers_db"
+	});
+};
+
+connection.connect();
 module.exports = connection;
